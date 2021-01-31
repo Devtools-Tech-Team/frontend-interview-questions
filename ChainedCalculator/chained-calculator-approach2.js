047f7cbd050a83b6ed578e3c1969e0f81037ba89
Puneet Ahuja <puneet.ahuja@olx.com>
2021-02-01 00:21:20 +0530

    Added Chained Calculator Approaches

---
+++ ChainedCalculator/chained-calculator-approach2.js
@@ -0,0 +1,76 @@
+/**
+ * For more programming concepts, questions, tips, and tutorials, visit:
+ * 
+ * https://bit.ly/devtools-yt
+ * https://code.devtools.tech
+ * https://devtools.tech
+ * 
+ * Author: Puneet Ahuja (https://puneetahuja.in)
+ */
+
+ /**
+  * Question: Create a function calculator that should take one initial value.
+  * Chain calculations like add, subtract on it.
+  * Return the calculated value on invocation of val function at the end of the chain.
+  * 
+    var result = cal(2)
+            .add(5)
+            .sub(4)
+            .val()
+
+    console.log("Result is : " , result)
+
+    // Output : 3 (2 + 5 - 4)
+  */
+
+
+/**
+ * **********  Approach - 2  **********
+ */
+
+ 
+function cal( initialValue ){
+
+    console.log("Is this global?" , this === global)
+
+    // 1. create accumulator - store result.
+
+    // closure
+    var accumulator = initialValue;
+
+    var calObj = {
+        add,
+        sub,
+        val
+    }
+
+    // 2. Define add, sub and val functions .
+
+    function add(num){
+        accumulator += num;
+        return calObj;
+    }
+
+    function sub(num){
+        accumulator -= num;
+        return calObj;
+    }
+
+    function val(){
+        return accumulator;
+    }
+
+    // 3. Return an object with add, sub and val functions. - chain
+    return calObj;
+ }
+
+ console.log("Before cal call" , global.add)
+
+ var result = cal(2)
+ .add(5)
+ .sub(4)
+ .val()
+
+ console.log("After cal call" , global.add)
+
+console.log("Result is : " , result)
